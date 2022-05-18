f1e0fcbe2d18f1ea2e71c0e423d6cae268fc84b5
QuocTuanIT <kutuanonline199@gmail.com>
2022-05-18 21:44:29 +0700

    Fixed1

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-bf*a3sjlr*8g3hduc_dgpmw2)d&%_(x(_zg7^5i5zg(*28vtob
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['tqt-django-blog.herokuapp.com']
 
 
 # Application definition
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
